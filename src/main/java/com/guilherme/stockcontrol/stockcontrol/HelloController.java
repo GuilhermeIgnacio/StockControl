44df962d4326cb9695ad794956797c95b1f875b9
GuilhermeIgnacio <guilhermeignacio002@gmail.com>
2024-08-22 23:19:24 -0300

    Insert Item Window

--- src/main/java/com/guilherme/stockcontrol/stockcontrol/HelloController.java
+++ src/main/java/com/guilherme/stockcontrol/stockcontrol/HelloController.java
@@ -1,14 +1,32 @@
 package com.guilherme.stockcontrol.stockcontrol;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Button addNewItemBtn;
+
+    public void onAddNewItemBtnClicked(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("add-item-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Insert Item!");
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(addNewItemBtn.getScene().getWindow());
+        stage.show();
+
     }
 }
