984d9ef1c9e2fdfc98c7e37092a76d93bc5ed1a0
GuilhermeIgnacio <guilhermeignacio002@gmail.com>
2024-08-28 19:17:47 -0300

    Item Details Data

--- src/main/java/com/guilherme/stockcontrol/stockcontrol/ItemDetailController.java
+++ src/main/java/com/guilherme/stockcontrol/stockcontrol/ItemDetailController.java
@@ -1,9 +1,20 @@
 package com.guilherme.stockcontrol.stockcontrol;
 
 import com.guilherme.stockcontrol.stockcontrol.model.Item;
+import javafx.scene.control.Label;
 
 public class ItemDetailController {
-    public void getItem(Item item) {
+    public Label detailIdLabel;
+    public Label detailNameLabel;
+    public Label detailDescriptionLabel;
+    public Label detailQuantityLabel;
+    public Label detailPriceLabel;
 
+    public void getItem(Item item) {
+        detailIdLabel.setText(String.valueOf(item.getId()));
+        detailNameLabel.setText(item.getItemName());
+        detailDescriptionLabel.setText(item.getItemDescription());
+        detailQuantityLabel.setText(String.valueOf(item.getItemQuantity()));
+        detailPriceLabel.setText("$" + item.getItemPrice());
     }
 }
