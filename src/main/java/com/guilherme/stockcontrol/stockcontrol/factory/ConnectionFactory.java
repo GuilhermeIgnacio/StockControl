555fba6ead0f3bc7de13e696969de0bf27bd84d1
GuilhermeIgnacio <guilhermeignacio002@gmail.com>
2024-08-30 18:25:27 -0300

    Solve Deprecated Message

--- src/main/java/com/guilherme/stockcontrol/stockcontrol/factory/ConnectionFactory.java
+++ src/main/java/com/guilherme/stockcontrol/stockcontrol/factory/ConnectionFactory.java
@@ -12,7 +12,8 @@ public class ConnectionFactory {
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/StockDb";
 
     public static Connection createConnectionToMySql() throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
+
+        //Class.forName("com.mysql.jdbc.Driver"); Deprecated
 
         return DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
 
