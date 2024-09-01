30f4f5421433d2062af6863a91d62e5cea58cc8d
GuilhermeIgnacio <guilhermeignacio002@gmail.com>
2024-09-01 15:36:16 -0300

    Item Detail Overhaul

--- src/main/java/com/guilherme/stockcontrol/stockcontrol/ItemDetailController.java
+++ src/main/java/com/guilherme/stockcontrol/stockcontrol/ItemDetailController.java
@@ -12,7 +12,9 @@ public class ItemDetailController {
     public Label detailNameLabel;
     public Label detailDescriptionLabel;
     public Label detailQuantityLabel;
-    public Label detailPriceLabel;
+    public Label detailPurchasePriceLabel;
+    public Label detailRetailPriceLabel;
+    public Label detailProfitMarginLabel;
     public Label detailCreatedAtLabel;
     public Label detailUpdatedAtLabel;
 
@@ -20,10 +22,20 @@ public class ItemDetailController {
 
     public void getItem(Item item) {
         detailIdLabel.setText(String.valueOf(item.getId()));
+
         detailNameLabel.setText(item.getItemName());
         detailDescriptionLabel.setText(item.getItemDescription());
+
         detailQuantityLabel.setText(String.valueOf(item.getItemQuantity()));
-        detailPriceLabel.setText(currencyFormatter.format(item.getPurchasePrice()));
+
+        detailPurchasePriceLabel.setText(currencyFormatter.format(item.getPurchasePrice()));
+        detailRetailPriceLabel.setText(currencyFormatter.format(item.getRetailPrice()));
+
+        float profitMargin = ((item.getRetailPrice() - item.getPurchasePrice()) / item.getRetailPrice()) * 100f;
+
+        String formattedMarginProfit = String.format("%.1f%% (%s per unit)", profitMargin, currencyFormatter.format(item.getRetailPrice() - item.getPurchasePrice()));
+
+        detailProfitMarginLabel.setText(formattedMarginProfit);
 
         String formattedCreatedAt = item.getCreatedAt().format(dateTimeFormatter);
         String formattedUpdated = item.getUpdatedAt().format(dateTimeFormatter);
