de8ee119c1f6f1cd6316e57baeea3ccef92de509
Guilherme Ignácio <guilhermeignacio002@gmail.com>
2024-10-13 11:19:38 -0300

    Fetch Orders

--- src/main/java/com/guilherme/stockcontrol/stockcontrol/SalesController.java
+++ src/main/java/com/guilherme/stockcontrol/stockcontrol/SalesController.java
@@ -1,22 +1,29 @@
 package com.guilherme.stockcontrol.stockcontrol;
 
 import com.guilherme.stockcontrol.stockcontrol.dao.StockDAO;
-import com.guilherme.stockcontrol.stockcontrol.model.Sale;
+import com.guilherme.stockcontrol.stockcontrol.model.Product;
 import com.guilherme.stockcontrol.stockcontrol.model.SaleProduct;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 import java.net.URL;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import static com.guilherme.stockcontrol.stockcontrol.Util.currencyFormatter;
+import static com.guilherme.stockcontrol.stockcontrol.Util.dateTimeFormatter;
 
 public class SalesController implements Initializable {
 
@@ -24,36 +31,86 @@ public class SalesController implements Initializable {
     public Label monthIncomeLabel;
     public Label yearIncomeLabel;
     public VBox vBox;
+
+    public TableView tableView;
+
+    public TableColumn<SaleProduct, Date> saleDateColumn;
+    public TableColumn<SaleProduct, String> productNameColumn;
+    public TableColumn<SaleProduct, Float> salePriceColumn;
+
+    ObservableList<SaleProduct> saleProductsList = FXCollections.observableArrayList();
+
     StockDAO stockDAO = new StockDAO();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-//        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+        //Preenchendo Tabela
+        fetchSales();
+
+        saleDateColumn.setCellValueFactory(new PropertyValueFactory<>("saleDate"));
+        productNameColumn.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        salePriceColumn.setCellValueFactory(new PropertyValueFactory<>("salePrice"));
+
+        saleDateColumn.setCellFactory(_ -> new TableCell<>() {
+            private final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+            @Override
+            protected void updateItem(Date item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(formatter.format(item));
+                }
+            }
+        });
+
+        salePriceColumn.setCellFactory(_ -> new TableCell<>() {
+
+            @Override
+            protected void updateItem(Float item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(currencyFormatter.format(item));
+                }
+            }
+        });
+
+        //Preenchendo Tabela
 
         totalIncomeLabel.setText(currencyFormatter.format(stockDAO.totalIncome()));
         monthIncomeLabel.setText(currencyFormatter.format(stockDAO.monthIncome()));
         yearIncomeLabel.setText(currencyFormatter.format(stockDAO.yearIncome()));
 
-        for (SaleProduct saleProduct : stockDAO.fetchSaleProduct("", "", "")) {
+    }
 
-            HBox hBox = new HBox();
-            hBox.setSpacing(10);
+    public void fetchSales() {
 
-            Label productNameLabel = new Label(saleProduct.getProductName());
-            Label salePriceLabel = new Label(currencyFormatter.format(saleProduct.getSalePrice()));
-            Label saleDateLabel = new Label(saleProduct.getSaleDate().toString());
+        try {
+            saleProductsList.clear();
 
-            Label dotLabel = new Label("•");
-            Label dotLabel2 = new Label("•");
+            for (SaleProduct saleProduct : stockDAO.fetchSaleProduct("", "", "")) {
 
-            hBox.setAlignment(Pos.CENTER);
-            hBox.getChildren().addAll(productNameLabel, dotLabel, salePriceLabel, dotLabel2, saleDateLabel);
+                SaleProduct newSaleProduct = new SaleProduct();
 
-            vBox.getChildren().add(hBox);
+                newSaleProduct.setSaleId(saleProduct.getSaleId());
+                newSaleProduct.setProductName(saleProduct.getProductName());
+                newSaleProduct.setSalePrice(saleProduct.getSalePrice());
+                newSaleProduct.setSaleDate(saleProduct.getSaleDate());
 
+                saleProductsList.add(newSaleProduct);
+                tableView.setItems(saleProductsList);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
 
     }
+
 }
