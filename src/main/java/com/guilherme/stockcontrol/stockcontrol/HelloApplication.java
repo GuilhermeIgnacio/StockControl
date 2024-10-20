99dba6334f6d7c0d0f7fc00c35b37cdc2bbef941
Guilherme Ignácio <guilhermeignacio002@gmail.com>
2024-10-20 17:09:38 -0300

    Commented HelloApplication and HelloController

--- src/main/java/com/guilherme/stockcontrol/stockcontrol/HelloApplication.java
+++ src/main/java/com/guilherme/stockcontrol/stockcontrol/HelloApplication.java
@@ -5,22 +5,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 import static com.guilherme.stockcontrol.stockcontrol.Util.getProp;
+
+/**
+ * Classe principal do aplicativo JavaFX.
+ * Extende a classe Application e inicializa a interface gráfica.
+ */
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        // Carrega o arquivo FXML da interface do usuário
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        //Define as Strings que estão em string.properties
         fxmlLoader.setResources(getProp());
+
+        // Cria uma nova cena com o layout carregado e define o tamanho da janela
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
+
+        // Define o título da janela principal usando uma propriedade localizada
         stage.setTitle(getProp().getString("main.window.title"));
         stage.setScene(scene);
-        stage.show();
+        stage.show(); //Exibe a janela
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(); // Inicia o aplicativo JavaFX
     }
 }
--- src/main/java/com/guilherme/stockcontrol/stockcontrol/HelloController.java
+++ src/main/java/com/guilherme/stockcontrol/stockcontrol/HelloController.java
@@ -1,43 +1,73 @@
 package com.guilherme.stockcontrol.stockcontrol;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
-import static com.guilherme.stockcontrol.stockcontrol.Util.getProp;
+import static com.guilherme.stockcontrol.stockcontrol.Util.genericAlertDialog;
 import static com.guilherme.stockcontrol.stockcontrol.Util.loadContent;
+
+/**
+ * Controlador da interface do usuário para a aplicação Hello.
+ * Esta classe é responsável por gerenciar eventos e a lógica de navegação entre as diferentes telas.
+ */
 
 public class HelloController implements Initializable {
 
-    public Button statisticsBtn;
-    public VBox contentArea;
-    public Button homeBtn;
+    public Button statisticsBtn; //Todo: Remove this unused reference
+    public VBox contentArea; // Área de conteúdo onde as views são carregadas
+    public Button homeBtn; //Todo: Remove this unused reference
+
+    /**
+     * Inicializa o controlador após seu elemento raiz tiver sido completamente processado
+     */
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         try {
-            loadContent("home-view.fxml", contentArea);
+            loadContent("home-view.fxml", contentArea); // Carrega a view inicial
         } catch (Exception e) {
+            //Todo: Extract Error Message to string.properties
+            genericAlertDialog(Alert.AlertType.ERROR, "", "Error Loadign Home View", e.getMessage());
             throw new RuntimeException(e);
         }
 
     }
 
+    /**
+     * Manipulador de eventos para o botão de estatísticas.
+     * Carrega a view de estatísticas na área de conteúdo.
+     *
+     * @param actionEvent O evento gerado pelo clique do botão.
+     */
+
     public void onStatisticsBtnClicked(ActionEvent actionEvent) throws Exception {
         loadContent("statistics-view.fxml", contentArea);
     }
 
+    /**
+     * Manipulador de eventos para o botão inicial.
+     * Carrega a view inicial na área de conteúdo.
+     *
+     * @param actionEvent O evento gerado pelo clique do botão.
+     */
+
     public void onHomeBtnClicked(ActionEvent actionEvent) throws Exception {
         loadContent("home-view.fxml", contentArea);
     }
+
+    /**
+     * Manipulador de eventos para o botão de vendas.
+     * Carrega a view de vendas na área de conteúdo.
+     *
+     * @param actionEvent O evento gerado pelo clique do botão.
+     */
 
     public void onSalesButtonClicked(ActionEvent actionEvent) throws Exception {
         loadContent("sales-view.fxml", contentArea);
